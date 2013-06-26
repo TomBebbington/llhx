a312b9765e0e9b7212557ae55b51ce8a312c51e3
Tom Bebbington <tophattedcoder@gmail.com>
2013-06-26 20:55:51 +0100

    Clearer names

--- Test.hx
+++ Test.hx
@@ -1,11 +1,11 @@
 class Testy implements llhx.LowLevel {
-	public static function diameter(r:Float):Float {
+	public static function circumference(r:Float):Float {
 		return r * Math.PI * 2.0;
 	}
 }
 class Test {
 	public static function main() {
-		var r = Testy.diameter(10);
+		var r = Testy.circumference(10);
 		trace(r);
 	}
 }
